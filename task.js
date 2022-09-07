3652b20c29fac91e2f9c1cd61e1e629213cf0339
wkurbatow <102980285+wkurbatow@users.noreply.github.com>
2022-09-07 16:21:34 +0300

    Update task.js

--- task.js
+++ task.js
@@ -7,3 +7,20 @@ unicArr = [...new Set(arr)].sort((a, b) => a - b);
 console.log(unicArr);
 newArr = unicArr.map((x) => arr.filter((name) => name === x));
 console.log(newArr);
+
+
+найти значения которых нет в обоих массивах
+на примере ответ 1,2,4
+
+const first = [1, 3, 3, 4, 6, 5, 4];
+const second = [6, 3, 5, 2, 2];
+
+const findDifferent = (first, second) => {
+  const x = first.filter((elem) => !second.includes(elem));
+  const y = second.filter((elem) => !first.includes(elem));
+  fin = [...new Set(x.concat(y))].sort();
+
+  return fin;
+};
+
+console.log(findDifferent(first, second));
